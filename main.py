c450bb36be2f03225537dc9e4525eacb0a3e52cd
DaniloNicacio <danlonicacio2016@gmail.com>
2023-08-08 12:49:09 -0400

    modified:   main.py 	new file:   people.json

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import json
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Person(BaseModel):
+    id: Optional[int] = None
+    name: str
+    age: int
+    gender: str
+    
+with open("people.json", "r") as f:
+    people = json.load(f)["people"]
+    
+print(people)
